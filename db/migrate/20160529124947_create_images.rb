fd7b4c4424917c0996fcbf90c6bed10ff545265f
Minkyu Kim <minkyu.kim@devsisters.com>
2016-05-29 21:54:33 +0900

    Remove images

--- db/migrate/20160529124947_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration
-  def change
-    create_table :images do |t|
-      t.string :path
-      t.references :lesson
-      t.timestamps null: false
-    end
-  end
-end
