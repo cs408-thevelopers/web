8e66911a42f76502d73dd4cf83c235155cac09d2
Minkyu Kim <minkyu.kim@devsisters.com>
2016-06-09 23:32:59 +0900

    Sort filenames

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,12 +3,12 @@ class Lesson < ActiveRecord::Base
   def images
     Dir["#{Rails.root}/public/images/#{id}/*.jpg"].map do |path|
       "#{id}/#{File.basename path}"
-    end
+    end.sort
   end
 
   def pdfs
     Dir["#{Rails.root}/public/images/#{id}/*.pdf"].map do |path|
       "#{id}/#{File.basename path}"
-    end
+    end.sort
   end
 end
